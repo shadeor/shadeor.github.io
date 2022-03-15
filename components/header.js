784614d3f4d49378a9237c43d82703893685e2f3
shadeor <31643991+shadeor@users.noreply.github.com>
2022-03-15 19:37:01 -0400

    Updated Navigation
    Updated Navigation in it own component file. Used JS to render navigation on site pages.

---
+++ components/header.js
@@ -0,0 +1,36 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+<nav class="main-nav">
+<a class="current"href="index.html">Portfolio</a>
+<a href="about.html">About</a>    
+</nav>
+
+<div id="side-nav" class="side-nav hidden">
+    <ul>
+        <li class="item"><a href="games.html">Games</a></li>
+        <li class="item"><a href="projects.html">Projects</a></li>
+        <li class="item"><a href="art.html">Art</a></li>
+    </ul>
+</div>
+
+<div class="side-bar">
+<div id="hamburger" class="hamburger" onclick="toggleNav()">
+    <div></div> 
+    <div></div>
+    <div></div>
+</div>
+
+
+
+<a href="mailto:shadeor@miamioh.edu" class="rotate">Shadeor@miamioh.edu</a>    
+<a href="https://www.linkedin.com/in/oliviashade/" target="_blank" rel="noopener noreferrer "><img alt="linkedin" src="images/linkedin-icon.png" class="icon"></a>
+</div>
+`;
+}
+}
+customElements.define('header-component', Header);
